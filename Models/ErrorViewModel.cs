880a9c335c8c2dd9649adc6a4be5a0dbcbad0655
Yunusa <yyohanna19@gmail.com>
2024-10-01 20:30:03 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicketMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
