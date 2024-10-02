a71e5bed27d8c22a2d4448406d450be81abad601
Yunusa <yyohanna19@gmail.com>
2024-10-02 13:43:53 +0100

    Added Actors View Successfully

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -14,7 +14,7 @@ namespace eTicketMvcApp.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -6,8 +6,14 @@ namespace eTicketMvcApp.Models
     {
         [Key]
         public int  ActorId { get; set; }
+
+        [Display(Name = "Profile Picture URL")]
         public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "FullName")]
         public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
         //Relationships
