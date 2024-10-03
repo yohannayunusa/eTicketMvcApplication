08082f7c635085ed7b431eec15e59a18a0eb1c4f
Yunusa <yyohanna19@gmail.com>
2024-10-03 12:07:17 +0100

    Successfully implemented getting actors details by Id

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace eTicketMvcApp.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -33,8 +33,17 @@ namespace eTicketMvcApp.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+            await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("Empty");
+            return View(actorDetails);
         }
     }
 }
